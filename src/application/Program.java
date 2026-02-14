2dedeb8bab20abe972e11e08d150a10d8b71919b
felipe ross <kamigamink@gmail.com>
2026-02-14 18:29:12 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,14 +9,6 @@ public class Program {
 	public static void main(String[] args) {
 		
 		
-		Board board = new Board(8,8);
-		
-		System.out.println(board);
-		
-		Position position = new Position(3,5);
-		
-		System.out.println(position);
-		
 		ChessMatch match = new ChessMatch();
 		UI.printBoard(match.getPieces());
 	}
